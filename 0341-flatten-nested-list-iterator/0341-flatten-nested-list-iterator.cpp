ac148ed1f56276b688ebd6ee4524f55f5a1664c5
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2023-10-20 21:15:07 +0530

    Time: 4 ms (78.27%), Space: 14.9 MB (27.19%) - LeetHub

---
+++ 0341-flatten-nested-list-iterator/0341-flatten-nested-list-iterator.cpp
@@ -0,0 +1,65 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * class NestedInteger {
+ *   public:
+ *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     bool isInteger() const;
+ *
+ *     // Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // The result is undefined if this NestedInteger holds a nested list
+ *     int getInteger() const;
+ *
+ *     // Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // The result is undefined if this NestedInteger holds a single integer
+ *     const vector<NestedInteger> &getList() const;
+ * };
+ */
+
+#include <vector>
+#include <stack>
+
+class NestedIterator {
+public:
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        // Initialize the stack with a reverse order of nestedList
+        for (int i = nestedList.size() - 1; i >= 0; i--) {
+            stack_.push(nestedList[i]);
+        }
+    }
+
+    int next() {
+        // Ensure that the hasNext function is called before next
+        if (!hasNext()) {
+            // You can throw an exception or return a default value as required
+        }
+        int result = stack_.top().getInteger();
+        stack_.pop();
+        return result;
+    }
+
+    bool hasNext() {
+        // While the stack is not empty, and the top element is not an integer
+        while (!stack_.empty() && !stack_.top().isInteger()) {
+            // Get the top list and pop it from the stack
+            vector<NestedInteger> topList = stack_.top().getList();
+            stack_.pop();
+            // Push its elements onto the stack in reverse order
+            for (int i = topList.size() - 1; i >= 0; i--) {
+                stack_.push(topList[i]);
+            }
+        }
+        // Check if the stack is not empty
+        return !stack_.empty();
+    }
+
+private:
+    stack<NestedInteger> stack_;
+};
+
+
+/**
+ * Your NestedIterator object will be instantiated and called as such:
+ * NestedIterator i(nestedList);
+ * while (i.hasNext()) cout << i.next();
+ */
