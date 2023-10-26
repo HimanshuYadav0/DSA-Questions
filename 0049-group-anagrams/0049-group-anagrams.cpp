a90bff7bcb2274701320ff3e844be4aa2fa66789
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2023-10-26 22:29:33 +0530

    Time: 20 ms (96.65%), Space: 20.2 MB (72.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<vector<string>> result;
         unordered_map<string, vector<string>> anagramGroups;
         
         for (const string& str : strs) {
-            string sortedStr = str; 
+            string sortedStr = str;
             sort(sortedStr.begin(), sortedStr.end()); 
-            
-            if (anagramGroups.find(sortedStr) == anagramGroups.end()) {
-                anagramGroups[sortedStr] = {str};
-            } else {
-                anagramGroups[sortedStr].push_back(str);
-            }
+            anagramGroups[sortedStr].push_back(str);
         }
         
-        for (const auto& group : anagramGroups) {
-            result.push_back(group.second);
+        vector<vector<string>> result;
+        for (auto it = anagramGroups.begin(); it != anagramGroups.end(); ++it) {
+            result.push_back(it->second);
         }
         
         return result;
