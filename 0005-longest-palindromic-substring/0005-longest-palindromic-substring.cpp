6bfcc1e7632e919c4c9a1bd2fa34dd724e2e65e0
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2023-10-28 00:28:21 +0530

    Time: 188 ms (37.10%), Space: 23.4 MB (33.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.length();
-        if (n <= 1) {
+        if (n < 2) {
             return s;
         }
 
         vector<vector<bool>> dp(n, vector<bool>(n, false));
-        int start = 0, maxLen = 1;
+        int start = 0; 
+        int maxLen = 1; 
 
         for (int i = 0; i < n; i++) {
             dp[i][i] = true;
         }
 
+        // Check for substrings of length 2
         for (int i = 0; i < n - 1; i++) {
             if (s[i] == s[i + 1]) {
                 dp[i][i + 1] = true;
