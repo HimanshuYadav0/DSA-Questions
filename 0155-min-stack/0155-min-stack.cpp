811d213ca50be4ad61d420a71293acba2ee7aac2
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2023-12-03 11:09:44 +0530

    Time: 24 ms (17.09%), Space: 16.9 MB (22.52%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,36 @@
+
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        
+        if (minStack.empty() || val <= minStack.top()) {
+            minStack.push(val);
+        }
+    }
+    
+    void pop() {
+        if (mainStack.top() == minStack.top()) {
+            minStack.pop();
+        }
+        
+        mainStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+
+private:
+    std::stack<int> mainStack;  
+    std::stack<int> minStack;   
+};
+
