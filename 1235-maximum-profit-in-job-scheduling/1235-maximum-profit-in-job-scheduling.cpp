dc8b2143f1de3cf7d4afd50e53f653e3b1cca7f0
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2024-01-06 15:11:19 +0530

    Time: 124 ms (85.44%), Space: 66.1 MB (83.65%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,5 +1,3 @@
-#include <vector>
-#include <algorithm>
 
 class Solution {
 public:
@@ -7,25 +5,23 @@ public:
         int n = startTime.size();
         vector<tuple<int, int, int>> jobs;
 
-        // Combine information into a jobs array
+
         for (int i = 0; i < n; ++i) {
             jobs.push_back({startTime[i], endTime[i], profit[i]});
         }
 
-        // Sort jobs based on end time
+
         sort(jobs.begin(), jobs.end(), [](const auto& a, const auto& b) {
             return get<1>(a) < get<1>(b);
         });
 
-        // Initialize dp array
+
         vector<int> dp(n, 0);
 
         // Base case
         dp[0] = get<2>(jobs[0]);
 
-        // Iterate through jobs and update dp array
         for (int i = 1; i < n; ++i) {
-            // Binary search to find the latest job that doesn't overlap with the current job
             int low = 0, high = i - 1;
             while (low <= high) {
                 int mid = (low + high) / 2;
@@ -41,13 +37,11 @@ public:
                 }
             }
 
-            // If no non-overlapping job is found
             if (low > high) {
                 dp[i] = max(get<2>(jobs[i]), dp[i - 1]);
             }
         }
 
-        // The final answer is the maximum value in the dp array
         return dp[n - 1];
     }
 };
