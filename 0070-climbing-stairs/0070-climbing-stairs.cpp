f420453494efc1c016f5673fe81117a9435e62fe
Himanshu Yadav <103452334+HimanshuYadav0@users.noreply.github.com>
2024-01-18 19:24:17 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (35.64%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -5,19 +5,15 @@ public:
             return n;
         }
         
-        // Initialize an array to store the number of ways to reach each step.
         vector<int> dp(n + 1, 0);
         
-        // There are 2 ways to reach the first and second steps.
         dp[1] = 1;
         dp[2] = 2;
         
-        // Calculate the number of ways to reach each step from step 3 to n.
         for (int i = 3; i <= n; i++) {
             dp[i] = dp[i - 1] + dp[i - 2];
         }
         
-        // The result is stored in dp[n].
         return dp[n];
     }
 };
